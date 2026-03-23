a26348d0dee0932fd26daf6f19e1f08d22342498
Andrea Fiori <afiori@terranovasoftware.eu>
2026-03-23 16:21:25 +0100

    Task tracker updated

--- task-tracker/script.js
+++ task-tracker/script.js
@@ -50,14 +50,16 @@ class TaskManager {
 
             const taskDescription = document.createElement('label');
             taskDescription.setAttribute('for', 'task_' + index);
+            taskDescription.setAttribute('class', 'checkbox-container');
             taskDescription.textContent = task.description;
+            taskDescription.appendChild(checkbox);
+            taskDescription.appendChild(document.createElement('span')).classList.add('checkmark');
 
             const deleteBtn = document.createElement('button');
-            deleteBtn.innerHTML = '<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.org/2000/svg"><path d="M5.5 1C5.22386 1 5 1.22386 5 1.5C5 1.77614 5.22386 2 5.5 2H9.5C9.77614 2 10 1.77614 10 1.5C10 1.22386 9.77614 1 9.5 1H5.5ZM3 3.5C3 3.22386 3.22386 3 3.5 3H5H10H11.5C11.7761 3 12 3.22386 12 3.5C12 3.77614 11.7761 4 11.5 4H11V12C11 12.5523 10.5523 13 10 13H5C4.44772 13 4 12.5523 4 12V4L3.5 4C3.22386 4 3 3.77614 3 3.5ZM5 4H10V12H5V4Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>';
+            deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
             deleteBtn.classList.add('delete-btn');
             deleteBtn.addEventListener('click', () => this.deleteTask(this.tasks.indexOf(task)));
 
-            taskItem.appendChild(checkbox);
             taskItem.appendChild(taskDescription);
             taskItem.appendChild(deleteBtn);
 
